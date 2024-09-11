d69fea5f5882cb746c9cb75daa8a29e1ac715106
RUPAK <rupak@RUPAKs-Laptop.local>
2024-09-11 23:43:24 +0600

    remvoe element

--- src/com/practice/array/ArrayBasic.java
+++ src/com/practice/array/ArrayBasic.java
@@ -1,8 +1,45 @@
 package com.practice.array;
 
+import java.util.Arrays;
+
 public class ArrayBasic {
 
     public static void main(String[] args) {
+        int[] arr = {2, 3, 4, 1, 0, 6};
+       // Arrays.sort(arr);
 
+/*      int counter = 0;
+        for (int a : arr) {
+            counter++;
+        }
+        System.out.println("Array Length : "+counter);*/
+
+        System.out.println(Arrays.toString(arr));
+        int elementToRemove = 1;
+        removeElement(arr, elementToRemove);
+        System.out.println(Arrays.toString(arr));
     }
+
+    /**
+     * remove provided element from the array
+     */
+    private static void removeElement(int[] arr, int elementToRemove) {
+        int index = -1;
+        int arrLength = arr.length;
+        for(int i=0; i<arrLength; i++) {
+            if(arr[i] == elementToRemove) {
+                index = i;
+            }
+        }
+        if(index == -1) {
+            System.out.println(elementToRemove + " not found in the array!");
+            return;
+        }
+
+        for(int i=index; i<arrLength-1; i++) {
+            arr[i] = arr[i+1];
+        }
+    }
+
+
 }
