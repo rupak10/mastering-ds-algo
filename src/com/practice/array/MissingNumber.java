2e336637c1eacc215bf241c5bd3b3b320b9f7113
Rupak Kumar Das (Assistant Manager) <rupak@pksf.local>
2024-09-24 16:24:49 +0600

    missing number

--- src/com/practice/array/MissingNumber.java
+++ src/com/practice/array/MissingNumber.java
@@ -2,6 +2,16 @@ package com.practice.array;
 
 public class MissingNumber {
     public static void main(String[] args) {
+        int[] nums = {1};
+        System.out.println(missingNumber(nums));
+    }
 
+    private static int missingNumber(int[] nums) {
+        int arrLength = nums.length;
+        int sum = (arrLength*(arrLength + 1))/2;
+        for (int i = 0; i < arrLength; i++) {
+            sum -= nums[i];
+        }
+        return sum;
     }
 }
